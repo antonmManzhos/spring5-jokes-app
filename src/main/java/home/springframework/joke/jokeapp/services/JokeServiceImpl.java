cad841f7f22d0cbe4afb81cd7100b7a354850233
Anton <antonm1.manzhos@gmail.com>
2020-07-07 09:40:47 +0300

    added spring service layer

---
+++ src/main/java/home/springframework/joke/jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,17 @@
+package home.springframework.joke.jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+}
